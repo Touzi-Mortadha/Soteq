70334328d87e3ecede991bcc7db244ccffa229af
wafa yahayoui <hoffa.yahyaoui@gmail.com>
2017-06-03 23:56:00 +0100

    testing if the user is authetified

--- soteq/views.py
+++ soteq/views.py
@@ -7,41 +7,24 @@ class index_view(TemplateView):
     template_name = "index.html"
     def get_context_data(self, **kwargs):
         context = super(index_view, self).get_context_data(**kwargs)
-        context['connected'] = self.request.user.is_authenticated()
         context['object_list']=self.request.POST.get("content")
         return context
-
 
 class produits_view(ListView):
     template_name="produits.html"
     model=Produit
     paginate_by = 3
-    def get_context_data(self, **kwargs):
-        context = super(produits_view, self).get_context_data(**kwargs)
-        context['connected'] = self.request.user.is_authenticated()
-        return context
-
 
 class projects_view(TemplateView):
     template_name = "projects.html"
-    def get_context_data(self, **kwargs):
-        context = super(projects_view, self).get_context_data(**kwargs)
-        context['connected'] = self.request.user.is_authenticated()
-        return context
 
 class SAV_view(TemplateView):
     template_name = "SAV.html"
-    def get_context_data(self, **kwargs):
-        context = super(SAV_view, self).get_context_data(**kwargs)
-        context['connected'] = self.request.user.is_authenticated()
-        return context
+
 
 class contact_view(TemplateView):
     template_name = "contact_us.html"
-    def get_context_data(self, **kwargs):
-        context = super(contact_view, self).get_context_data(**kwargs)
-        context['connected'] = self.request.user.is_authenticated()
-        return context
+
 
 
 class produit_detail_view(TemplateView):
@@ -49,6 +32,5 @@ class produit_detail_view(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(produit_detail_view, self).get_context_data(**kwargs)
-        context['connected'] = self.request.user.is_authenticated()
         context['instance'] = get_object_or_404(Produit, id_produit=self.kwargs['id'])
         return context
