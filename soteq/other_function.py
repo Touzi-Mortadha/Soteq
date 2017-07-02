895b6cf0ad61f11921690f9f5dcd449cbf8d57c1
Touzi-Mortadha <touzimortadha1@gmail.com>
2017-07-02 21:16:06 +0100

    raka7t él search

--- soteq/other_function.py
+++ soteq/other_function.py
@@ -1,18 +1,33 @@
 from .models import *
 from django.db.models import Q
 from itertools import chain
+import re
+
 
 def recherche(query):
-    queryset_list1 = Produit.objects.filter(
-        Q(nom_produit__icontains=query) |
-        Q(description__icontains=query) |
-        Q(categorie__icontains=query)
-    ).distinct()
+    words = re.split(r"[^A-Za-z']+", query)
+    q = Q()  # empty Q object
+    for word in words:
+        # 'or' the queries together
+        q |= (Q(nom_produit__icontains=word) |
+              Q(description__icontains=word) |
+              Q(categorie__icontains=word))
+    queryset_list1 = Produit.objects.filter(q).all()
+
+    # queryset_list1 = Produit.objects.filter(
+    #     Q(nom_produit__icontains=query) |
+    #     Q(description__icontains=query) |
+    #     Q(categorie__icontains=query)
+    # ).distinct()
+
+
+
     # queryset_list2 = Projet.objects.filter(
     #     Q(nom_projet__icontains=query)
     # ).distinct()
     # queryset_list2 = chain(queryset_list1,queryset_list2)
     return queryset_list1
+
 
 def is_connected(request):
     if not request.user.is_staff or not request.user.is_superuser:
